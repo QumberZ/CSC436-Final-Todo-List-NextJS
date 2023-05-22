64805b1ae94d893bceecfdbd6c15bc03a878df29
Qumber Zaidi <Qumber.Zaidi@cix.csi.cuny.edu>
2023-05-22 01:57:08 -0400

    delete auto refresh

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -14,7 +14,24 @@ import Backdrop from "@mui/material/Backdrop";
 import { Box, Fade, TextField, Typography } from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
+// import PropTypes from 'prop-types';
+// import LinearProgress from '@mui/material/LinearProgress';
 
+
+// function LinearProgressWithLabel(props) {
+//   return (
+//     <Box sx={{ display: 'flex', alignItems: 'center' }}>
+//       <Box sx={{ width: '100%', mr: 1 }}>
+//         <LinearProgress variant="determinate" {...props} />
+//       </Box>
+//       <Box sx={{ minWidth: 35 }}>
+//         <Typography variant="body2" color="text.secondary">{`${Math.round(
+//           props.value,
+//         )}%`}</Typography>
+//       </Box>
+//     </Box>
+//   );
+// }
 
 const style = {
   position: "absolute",
@@ -38,7 +55,6 @@ const Profile = () => {
   const [deleteModalOpen, setDeleteModalOpen] = useState(false); // Track delete confirmation modal open state
   const [selectedItemToDelete, setSelectedItemToDelete] = useState(null); // Track selected item for deletion
 
-  
   const handleOpen = (item = null) => {
     if (item) {
       setSelectedItem(item);
@@ -59,31 +75,32 @@ const Profile = () => {
     setTasks([]);
   };
 
-
   const handleDeleteOpen = (item) => {
-  setSelectedItemToDelete(item);
-  setDeleteModalOpen(true);
-};
+    setSelectedItemToDelete(item);
+    setDeleteModalOpen(true);
+  };
 
-const handleDeleteClose = () => {
-  setSelectedItemToDelete(null);
-  setDeleteModalOpen(false);
-};
+  const handleDeleteClose = () => {
+    setSelectedItemToDelete(null);
+    setDeleteModalOpen(false);
+  };
 
-const deleteTodoList = async (e) => {
-  e.preventDefault();
-  if (selectedItemToDelete) {
-    const result = await deleteTodoItem(selectedItemToDelete.id);
-    if (result.success) {
-      const updatedItems = todoItems.filter((item) => item.id !== selectedItemToDelete.id);
-      setTodoItems(updatedItems);
-    } else {
-      // Handle error
+  const deleteTodoList = async (e) => {
+    e.preventDefault();
+    if (selectedItemToDelete) {
+      const result = await deleteTodoItem(selectedItemToDelete.id);
+      if (result.success) {
+        const updatedItems = todoItems.filter(
+          (item) => item.id !== selectedItemToDelete.id
+        );
+        setTodoItems(updatedItems);
+      } else {
+        // Handle error
+      }
     }
-  }
-  handleDeleteClose();
-};
-
+    handleDeleteClose();
+    refreshUser();
+  };
 
   const { user, refreshUser, error, loading } = useUser();
   useUserMustBeLogged(user, "in", "/login");
@@ -149,20 +166,18 @@ const deleteTodoList = async (e) => {
     // Handle success
   };
 
-
   const updateTaskCompletion = async (itemId, taskIndex, completed) => {
     const updatedTodoItems = [...todoItems];
     const itemIndex = updatedTodoItems.findIndex((item) => item.id === itemId);
     if (itemIndex !== -1) {
       updatedTodoItems[itemIndex].tasks[taskIndex].completed = completed;
       setTodoItems(updatedTodoItems);
-  
+
       // Update the completed status in Supabase or your preferred data source
       await updateTodoItem(itemId, updatedTodoItems[itemIndex].tasks);
     }
   };
 
-  
   const handleTaskChange = (e) => {
     setSelectedTask(e.target.value);
   };
@@ -318,59 +333,63 @@ const deleteTodoList = async (e) => {
                   </table>
 
                   <div className="flex justify-center mt-4">
-      <button
-        className="button small bg-blue-500 text-white font-bold mr-2"
-        onClick={() => handleOpen(item)} // Edit button
-      >
-        Edit
-      </button>
-      <button
-        className="button small bg-red-500 text-white font-bold"
-        onClick={() => handleDeleteOpen(item)} // Delete button
-      >
-        Delete
-      </button>
-    </div>
-  </div>
-))}
-<Modal
-  aria-labelledby="delete-confirmation-modal-title"
-  aria-describedby="delete-confirmation-modal-description"
-  open={deleteModalOpen}
-  onClose={handleDeleteClose}
-  closeAfterTransition
-  slots={{ backdrop: Backdrop }}
-  slotProps={{
-    backdrop: {
-      timeout: 500,
-    },
-  }}
->
-  <Fade in={deleteModalOpen}>
-    <Box sx={style}>
-      <Typography variant="h6" component="h2" mb={2}>
-        Are you sure you want to delete this todo list?
-      </Typography>
-      <Typography variant="body1" id="delete-confirmation-modal-description" mb={4}>
-        This action cannot be undone.
-      </Typography>
-      <div className="flex justify-end">
-        <button
-          className="button small bg-red-500 text-white font-bold"
-          onClick={deleteTodoList}
-        >
-          Delete
-        </button>
-        <button
-          className="button small bg-gray-500 text-white font-bold ml-2"
-          onClick={handleDeleteClose}
-        >
-          Cancel
-        </button>
-      </div>
-    </Box>
-  </Fade>
-</Modal>
+                    <button
+                      className="button small bg-blue-500 text-white font-bold mr-2"
+                      onClick={() => handleOpen(item)} // Edit button
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="button small bg-red-500 text-white font-bold"
+                      onClick={() => handleDeleteOpen(item)} // Delete button
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <Modal
+                aria-labelledby="delete-confirmation-modal-title"
+                aria-describedby="delete-confirmation-modal-description"
+                open={deleteModalOpen}
+                onClose={handleDeleteClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                  backdrop: {
+                    timeout: 500,
+                  },
+                }}
+              >
+                <Fade in={deleteModalOpen}>
+                  <Box sx={style}>
+                    <Typography variant="h6" component="h2" mb={2}>
+                      Are you sure you want to delete this todo list?
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      id="delete-confirmation-modal-description"
+                      mb={4}
+                    >
+                      This action cannot be undone.
+                    </Typography>
+                    <div className="flex justify-end">
+                      <button
+                        className="button small bg-red-500 text-white font-bold"
+                        onClick={deleteTodoList}
+                      >
+                        Delete
+                      </button>
+                      <button
+                        className="button small bg-gray-500 text-white font-bold ml-12"
+                        onClick={handleDeleteClose}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </Box>
+                </Fade>
+              </Modal>
             </div>
           </div>
         )}
